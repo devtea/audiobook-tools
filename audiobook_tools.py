378a4ae4f6a2fff5cedd8f7d199f3bd429d4503b
devtea <tdreyer1@gmail.com>
2024-12-29 15:31:07 -0600

    refactor organize_files

--- audiobook_tools.py
+++ audiobook_tools.py
@@ -13,6 +13,8 @@ import click
 CWD = os.getcwd()
 COMMON_CONTEXT = dict(help_option_names=["-h", "--help"])
 LOG = logging.getLogger(__name__)
+DIR_MODE = 0o777
+FILE_MODE = 0o666
 
 
 # decorator to add common logging level argument to click commands
@@ -34,15 +36,15 @@ def with_log_level(f):
 
 
 def prune_dir(dir: str):
-    LOG.info(f"Checking directory: {dir}")
+    LOG.debug(f"Checking directory: {dir}")
     try:
         os.rmdir(dir)
         LOG.info(f"Pruned empty directory {dir}")
     except OSError:
-        LOG.warning(f"Directory not empty when trying to prune: {dir}")
+        LOG.info(f"Directory not empty when trying to prune: {dir}")
 
 
-@click.group(context_settings=COMMON_CONTEXT, invoke_without_command=True)
+@click.group(context_settings=COMMON_CONTEXT)
 @with_log_level
 def cli():
     """CLI for automating file organization."""
@@ -70,43 +72,68 @@ def cli():
 )
 def organize_files(source: str, destination: str):
     """Organize files from source directory to destination directory."""
-    LOG.info(f"Destination: {destination}")
+    LOG.debug(f"Destination: {destination}")
+
+    os.umask(0o000)  # Eww.... but it's how the systems are currently configured to work
+
+    # create destination directory if it does not exist
+    try:
+        os.mkdir(destination)
+    except FileExistsError:
+        # This is fine, confirm perms
+        os.chmod(destination, DIR_MODE)
+
+    # pattern to match
+    pattern = re.compile(r"^([^-]*) - (.*).m4b$")
 
     # os walk through current dir and all subdirectories
     for root, dirs, files in os.walk(source, topdown=False):
         for file in files:
             LOG.info(f"Processing file: {file}")
-            # pattern to match
-            pattern = re.compile(r"^(.*) - (.*).m4b$")
             matches = pattern.findall(file)
-            LOG.info(f"Matches: {matches}")
+            LOG.debug(f"Matches: {matches}")
             if len(matches) > 1:
                 raise Exception("More than one match found")
             elif matches and len(matches[0]) == 2:
-                LOG.info(f"File split: {matches[0]}")
-                LOG.info(f"Root: {root}")
+                LOG.debug(f"File split: {matches[0]}")
+                LOG.debug(f"Root: {root}")
                 # create the new directory name
-                new_dir = matches[0][0]
+                author_name = matches[0][0]
+                LOG.debug(f"Extracted author name: {author_name}")
                 # create the new subdirectory name
-                new_subdir = matches[0][1]
+                title_name = matches[0][1]
+                LOG.debug(f"Extracted title name: {title_name}")
                 # create the new file name
                 new_file = file
-                # create the new file path
-                new_file_path = os.path.join(destination, new_dir, new_subdir, new_file)
-                # create the new directory path
-                new_dir_path = os.path.join(destination, new_dir, new_subdir)
-                # if the new directory does not exist, create it
-                if not os.path.exists(new_dir_path):
-                    os.makedirs(new_dir_path)
-                # move the file to the new file path
-                shutil.move(os.path.join(root, file), new_file_path)
+                LOG.debug(f"New file name: {new_file}")
+                author_dir = os.path.join(destination, author_name)
+                LOG.debug(f"Generated author directory: {author_dir}")
+                title_dir = os.path.join(author_dir, title_name)
+                LOG.debug(f"Generated title directory: {title_dir}")
+                old_file_path = os.path.join(root, file)
+                LOG.debug(f"Old file path: {old_file_path}")
+                new_file_path = os.path.join(title_dir, new_file)
+                LOG.debug(f"New file path: {new_file_path}")
 
-                # chmod the directories as 755
-                os.chmod(new_dir_path, 0o755)
-                # chmod the files as 644
-                os.chmod(new_file_path, 0o644)
+                # Create destination directories as needed
+                try:
+                    os.mkdir(author_dir)
+                except FileExistsError:
+                    # This is fine, confirm perms
+                    os.chmod(author_dir, DIR_MODE)
+                try:
+                    os.mkdir(title_dir)
+                except FileExistsError:
+                    # This is fine, confirm perms
+                    os.chmod(title_dir, DIR_MODE)
 
-        LOG.info("pruning empty directories.")
+                # move the file to the destination
+                LOG.info(f"Moving file {old_file_path} to {new_file_path}")
+                shutil.move(old_file_path, new_file_path)
+                # Set file permisisons
+                os.chmod(new_file_path, FILE_MODE)
+
+        LOG.debug("pruning empty directories.")
         for dir in dirs:
             prune_dir(os.path.join(root, dir))
         # prune the roots of each directory so long as it's not the cwd
